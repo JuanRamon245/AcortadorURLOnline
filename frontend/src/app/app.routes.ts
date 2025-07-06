93e209a6b281c18c37202b3b49fa8ab1c9972da4
JuanRamon245 <juanramondeleonmartin@gmail.com>
2025-07-06 20:33:07 +0200

    cambios visuales en el front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -16,7 +16,7 @@ export const routes: Routes = [
   { path: '', component: PaginaInicialComponent },
   { path: 'pagina-urlcorta-generada', component: PaginaURLCortaGeneradaComponent, canActivate: [UrlGuard] },
   { path: 'pagina-login', component: PaginaLoginComponent, canActivate: [noAuthGuard] },
-  { path: 'pagina-url-acortadas-usuario', component: PaginaUrlAcortadasUsuarioComponent, canActivate: [AuthGuard] },
+  { path: 'pagina-url-acortadas-usuario', component: PaginaUrlAcortadasUsuarioComponent },
   { path: 'pagina-redireccion-incorrecta', component: PaginaRedireccionIncorrectaComponent, canActivate: [RedireccionInvalidaGuard] },
   { path: 'verificar', component: VerificacionComponent , canActivate: [TokenVerificacionGuard] },
   { path: 'restablecer-contrasena', component: RestablecerContrasenaComponent , canActivate: [TokenVerificacionGuard] },
