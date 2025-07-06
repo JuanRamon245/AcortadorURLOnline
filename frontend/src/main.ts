96b9a3f3f8d93d54b20677dcf38b67cc473bc7bb
JuanRamon245 <juanramondeleonmartin@gmail.com>
2025-07-06 16:03:51 +0200

    Cambios en el front y en el back

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,11 +3,13 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
     { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 });
